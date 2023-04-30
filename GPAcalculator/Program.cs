51883168ce2e40b2ea055812da306d3a1d023ce7
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2023-04-30 23:27:36 +0100

    This my GPA Calculator

---
+++ GPAcalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GPAcalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You\'re welcome to my GPA Calculator");
+        }
+    }
+}
