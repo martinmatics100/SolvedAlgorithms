d8773402fc65099ba60251a56384123e7fe675d8
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2023-04-30 21:38:22 +0100

    solved algorithm questions
    added bubble sort and swap two numbers

---
+++ swap two numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SwapTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        int a = 7;
+        int b = 14;
+
+        // logic
+        int temp = a; // stored a as 7
+        a = b;
+        b = temp;
+
+        // result
+
+        Console.WriteLine($"a = {a} and b = {b}"); // result by interpolation
+        Console.WriteLine("a = " + a + " and b = " + b); // result by string concantenation
+        }
+    }
+}
