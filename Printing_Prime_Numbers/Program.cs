94db27383baff924509be9a34c73ff3b792276a8
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2023-04-30 23:02:36 +0100

    commit change

--- Printing_Prime_Numbers/Program.cs
+++ Printing_Prime_Numbers/Program.cs
@@ -1,11 +1,29 @@
-﻿using System.Globalization;
+﻿using System;
 
 namespace Printing_Prime_Numbers
 {
     internal class Program
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
+            int nums = 5000;
+            for (int i = 2; i < nums; i++) 
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
 
         }
     }
