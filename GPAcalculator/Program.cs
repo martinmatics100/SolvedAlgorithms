727db3f0305abc0d3133d70ff9cbd29f182667e3
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2023-05-01 20:27:36 +0100

    add changes

--- GPAcalculator/Program.cs
+++ GPAcalculator/Program.cs
@@ -4,74 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("You\'re welcome to my GPA Calculator");
+            //course data definitions
+            string[] courseCodes = { "MTH101", "GS101", "NET101", "C#101", "HC101"  };
+            int[] courseUnits = { 5, 3, 5, 5, 3 };
+            string[] grades = { "B", "A", "C", "B", "F" };
+            int[] gradeUnits = { 4, 5, 3, 4, 5 };
 
-            // variable initialization
-            int totalCourseUnit = 0;
-            int totalGradeUnit = 0;
-            int totalWeightPoint = 0;
+            // initialize total-grade-units and total-weight-points
+            int totalGradeUnits = 0;
+            int totalWeightPoints = 0;
 
-            // table header
-            Console.WriteLine("|-----------------------|----------------|-----------|---------------|---------------|----------------------| ");
-            Console.WriteLine("|    COURSES AND CODE   |    COURSE UNIT |   GRADE   |   GRADE-UNIT  |   WEIGHT-pt.  |   REMARK             | ");
-            Console.WriteLine("|-----------------------|----------------|-----------|---------------|---------------|----------------------| ");
-            Console.WriteLine("|        MTH101         |----------------|-----------|---------------|---------------|----------------------| ");
-            Console.WriteLine("|        GS101          |----------------|-----------|---------------|---------------|----------------------| ");
-            Console.WriteLine("|        NET101         |----------------|-----------|---------------|---------------|----------------------| ");
-            Console.WriteLine("|        C#101          |----------------|-----------|---------------|---------------|----------------------| ");
-            Console.WriteLine("|        HC101          |----------------|-----------|---------------|---------------|----------------------| ");
-
-            // adding course data's to the table
-
-            AddCourseToTable("MTH101", 5, "B", 4, ref totalCourseUnit, ref totalGradeUnit, ref totalWeightPoint);
-            AddCourseToTable("GS101", 3, "A", 5, ref totalCourseUnit, ref totalGradeUnit, ref totalWeightPoint);
-            AddCourseToTable("NET101", 5, "C", 3, ref totalCourseUnit, ref totalGradeUnit, ref totalWeightPoint);
-            AddCourseToTable("C#101", 5, "B", 4, ref totalCourseUnit, ref totalGradeUnit, ref totalWeightPoint);
-            AddCourseToTable("HC101", 3, "F", 0, ref totalCourseUnit, ref totalGradeUnit, ref totalWeightPoint);
-
-            // total footer
-           
-            // calculate and output the GPA
-
-            double gpa = (double)totalWeightPoint / totalGradeUnit;
-            Console.WriteLine($"\nTotal Course Unit Registered is {totalCourseUnit}");
-            Console.WriteLine($"\nTotal Course Unit Passed is {totalGradeUnit}");
-            Console.WriteLine($"\nTotal Weight Point is {totalWeightPoint}");
-            Console.WriteLine($"\nYour GPA is = {gpa:F2} to 2 decimal places.");
-        }
-        static void AddCourseToTable(string courseCode, int courseUnit, string grade, int gradeUnit, ref int totalCourseUnit, ref int totalGradeUnit, ref int totalWeightPoint)
-        {
-            int weightPoint = gradeUnit * courseUnit;
-            totalCourseUnit += courseUnit;
-
-            if (grade != "F")
+            //calculate weight points
+            for (int i = 0; i < courseCodes.Length; i++)
             {
-                totalGradeUnit += courseUnit;
-                totalWeightPoint += weightPoint;
+                int weightPoints = courseUnits[i] * gradeUnits[i];
+                totalGradeUnits += courseUnits[i];
+                totalWeightPoints += weightPoints;
             }
-            string remark = getRemark(grade);
-            Console.WriteLine($"|   {courseCode, - 14}    |   {courseUnit, - 13}    |   {grade, - 5}) |   {gradeUnit, - 11} |   {weightPoint, - 14}   |   {remark,--15}   |");
 
-            static string GetRemark(string grade)
+            //calculate GPA
+
+            double gpa = (double)totalWeightPoints / totalGradeUnits;
+
+            // table display
+            Console.WriteLine("|--------------------|-------------------|------------|--------------|---------------|-----------| ");
+            Console.WriteLine("|    COURSE & CODE   |   COURSE UNIT     |   GRADE    |  GRADE-UNIT  |   WEIGHT Pt.  |   REMARK  | ");
+            Console.WriteLine("|--------------------|-------------------|------------|--------------|---------------|-----------| ");
+            for (int i = 0; i < courseCodes.Length; i++)
             {
-                switch (grade)
-                {
-                    case "A":
-                        return "Excellent";
-                    case "B":
-                        return "Very Good";
-                    case "C":
-                        return "Good";
-                    case "D":
-                    case "E":
-                        return "Fair";
-                    default:
-                        return "Fail";
-
-
-
-                }
+             string remark = GetRemark(grades[i]);
+             int weightPoint = courseUnits[i] * gradeUnits[i];
+             Console.WriteLine($"| {courseCodes[i],-18} | {courseUnits[i],-17} | {grades[i],-10} | {gradeUnits[i],-12} | {weightPoint,-13} | {remark, -9} | ");
             }
+            Console.WriteLine("|--------------------|-------------------|------------|--------------|---------------|------------|");
+
+
+
+
         }
             
     }
