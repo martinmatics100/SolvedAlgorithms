6f2fe60b3dbc476491baa89ab140afdeb5d46c7f
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2023-05-03 10:58:34 +0100

    Print index
    You have been given an array consisting of integers. In addition you have been given an element, you need to find and print the index of the last occurrence of this element in the array if it exists in it, otherwise print -1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,8 +9,10 @@
             //you need to find and print the index of the last occurrence
             //of this element in the array if it exists in it, otherwise print -1.
 
+            //
+
             int[] arr = { 1, 2, 3, 4, 5, 6, 3, 2, 5, 4, 5, 6, 4, 5 };
-            int element = 25;
+            int element = 4;
             int lastIndex = -1;
 
             for (int i = 0; i < arr.Length; i++)
