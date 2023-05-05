a3699c99e058f2f299c85256bf0a0b37fc238c2f
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2023-05-05 10:55:55 +0100

    classes and objects

---
+++ ObjectAndClasses/Program.cs
@@ -0,0 +1,65 @@
+﻿// C# program to illustrate the
+// Initialization of an object
+using System;
+
+// Class Declaration
+public class Dog
+{
+
+    // Instance Variables
+    String name;
+    String breed;
+    int age;
+    String color;
+
+    // Constructor Declaration of Class
+    public Dog(String name, String breed,
+                int age, String color)
+    {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    // Property 1
+    public String GetName()
+    {
+        return name;
+    }
+
+    // Property 2
+    public String GetBreed()
+    {
+        return breed;
+    }
+
+    // Property 3
+    public int GetAge()
+    {
+        return age;
+    }
+
+    // Property 4
+    public String GetColor()
+    {
+        return color;
+    }
+
+    // Method 1
+    public String ToString()
+    {
+        return ("Hi my name is " + this.GetName()
+                + ".\nMy breed, age and color are " + this.GetBreed()
+                + ", " + this.GetAge() + ", " + this.GetColor());
+    }
+
+    // Main Method
+    public static void Main(String[] args)
+    {
+
+        // Creating object
+        Dog tuffy = new Dog("tuffy", "papillon", 5, "white");
+        Console.WriteLine(tuffy.ToString());
+    }
+}
