f7329590813197749cdf0eac24c0466870ec6562
Martinmatics100 <martinchukwuemeka67@gmail.com>
2023-05-19 19:38:21 +0100

    commit all

---
+++ WEEK_2_TASK/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WEEK_2_TASK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
