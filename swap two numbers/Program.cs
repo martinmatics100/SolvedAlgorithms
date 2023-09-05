a1983b146d7fd14700dc76361e4da7835705d712
Martinmatics100 <martinchukwuemeka67@gmail.com>
2023-09-05 15:35:35 +0100

    commit all

--- swap two numbers/Program.cs
+++ swap two numbers/Program.cs
@@ -4,18 +4,41 @@
     {
         static void Main(string[] args)
         {
-        int a = 7;
-        int b = 14;
+            //int a = 7;
+            //int b = 14;
 
-        // logic
-        int temp = a; // stored a as 7
-        a = b;
-        b = temp;
+            //// logic
+            //int temp = a; // stored a as 7
+            //a = b;
+            //b = temp;
 
-        // result
+            //// result
 
-        Console.WriteLine($"a = {a} and b = {b}"); // result by interpolation
-        Console.WriteLine("a = " + a + " and b = " + b); // result by string concantenation
+            //Console.WriteLine($"a = {a} and b = {b}"); // result by interpolation
+            //Console.WriteLine("a = " + a + " and b = " + b); // result by string concantenation
+
+            Console.WriteLine(GetName("Ifunanya", "Chukwudi"));
+
+            string fullName = GetName("Ifunanya", "Chukwudi");
+
+            int fullName2 = GetName(89, 67);
+
+            string fullName1 = GetName("Joe", "Doe");
+            Console.WriteLine($"My full name is {fullName}");
+            Console.WriteLine($"My full name is {fullName1}");
+            Console.WriteLine($"My full name is {fullName2}");
+
+
+        }
+
+        public static string GetName(string firstName, string lastName)
+        {
+            return firstName;
+        }
+        public static int GetName(int age, int height)
+        {
+            return height;
+            return age;
         }
     }
 }
