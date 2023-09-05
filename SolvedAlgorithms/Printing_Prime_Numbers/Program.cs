fe932c656622884a913f96e0221ea945d3c25151
Martinmatics100 <martinchukwuemeka67@gmail.com>
2023-09-05 16:17:33 +0100

    commit all

---
+++ SolvedAlgorithms/Printing_Prime_Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Printing_Prime_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = 2000;
+            for (int i = 2; i < nums; i++) 
+            {
+                bool isPrime = true;
+
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+
+        }
+    }
+}
